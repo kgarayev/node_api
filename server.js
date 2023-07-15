d807f9f52cadef254f86bb3c491638b720026dbf
kgarayev <kenangarayev@gmail.com>
2023-07-15 17:30:47 +0100

    minor bugs fixed

--- server.js
+++ server.js
@@ -1,26 +1,5 @@
-import { type } from "os";
-
 // importing express framework
 const express = require("express");
-
-// Importing types and creating an alias to avoid any naming conflict elsewhere in the project
-const {
-  Request: ExpressRequest,
-  Response: ExpressResponse,
-  Next: ExpressNext,
-} = require("express");
-
-// create a TS types for request, response and next
-type Request = typeof ExpressRequest;
-type Response = typeof ExpressResponse;
-type Next = typeof ExpressNext;
-
-// create a TS interface
-interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-}
 
 // importing api data
 const apiData = require("./apiData.json");
@@ -37,7 +16,7 @@ const myApp = express();
 myApp.use(express.static("public"));
 
 // a logging middleware
-myApp.use((req: Request, res: Response, next: Next) => {
+myApp.use((req, res, next) => {
   // show the date and the path
   console.log(new Date(), req.path);
 
@@ -46,22 +25,21 @@ myApp.use((req: Request, res: Response, next: Next) => {
 });
 
 // API KEY validation middleware
-myApp.use((req: Request, res: Response, next: Next) => {
-  console.log(apiKey);
-
+myApp.use((req, res, next) => {
   // defensive check to see if the API KEY entered matches the one in the system
   if (req.headers.api_key === apiKey) {
     // proceed if matches
     next();
+  } else {
+    // do not allow to proceed but respond
+    return res.send("Sorry, bad API key");
   }
-  // do not allow to proceed but respond
-  res.send("Sorry, bad API key");
 });
 
 // Mddleware section FINISH
 
 // route
-myApp.get("/users", (req: Request, res: Response) => {
+myApp.get("/users", (req, res) => {
   // log the headers of the request
   console.log("New request to: /", req.headers);
 
@@ -69,7 +47,7 @@ myApp.get("/users", (req: Request, res: Response) => {
   const { userName, num } = req.query;
 
   // add the id into the api data
-  apiData.users.forEach((user: User, index: number) => {
+  apiData.users.forEach((user, index) => {
     user.id = index + 1;
   });
 
@@ -79,7 +57,7 @@ myApp.get("/users", (req: Request, res: Response) => {
   // if a specific user is asked for
   if (req.query.userName) {
     // filter api data based on the request query
-    _apiData = _apiData.users.filter((user: User) => {
+    _apiData = _apiData.users.filter((user) => {
       // convert the first and last names to full name and lowercase
       const fullName = (
         user.firstName +
