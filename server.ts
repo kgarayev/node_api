4030fe02270cb075cf8991c359244db41b25aedb
kgarayev <kenangarayev@gmail.com>
2023-07-15 15:50:12 +0100

    simple defensive checks added to check number and user queries

--- server.ts
+++ server.ts
@@ -20,12 +20,19 @@ interface User {
 // creating a new instance of express
 const myApp = express();
 
+// Middleware
+// handle static files
+myApp.use(express.static("public"));
+
 // route
 myApp.get(
   "/users",
   (req: typeof ExpressRequest, res: typeof ExpressResponse) => {
     // log the headers of the request
     console.log("New request to: /", req.headers);
+
+    // destructuring the request queries
+    const { userName, num } = req.query;
 
     // add the id into the api data
     apiData.users.forEach((user: User, index: number) => {
@@ -35,15 +42,8 @@ myApp.get(
     // make a copy of api json data
     let _apiData = { ...apiData };
 
-    // if a specific quantity is asked for
-    if (req.query.num) {
-      _apiData.users.length = req.query.num;
-    }
-
     // if a specific user is asked for
     if (req.query.userName) {
-      console.log(req.query);
-
       // filter api data based on the request query
       _apiData = _apiData.users.filter((user: User) => {
         // convert the first and last names to full name and lowercase
@@ -54,17 +54,20 @@ myApp.get(
         ).toLocaleLowerCase();
 
         // return true or fase depending if the full name includes the requested name
-        return fullName.includes(req.query.userName.toLocaleLowerCase());
+        return fullName.includes(userName.toLocaleLowerCase());
       });
+    }
+
+    // if a specific quantity is asked for
+    if (num && Number(num) > 0 && Number(num) < _apiData.users.length) {
+      // chop the array to have the specified length
+      _apiData.users.length = num;
     }
 
     // send the response to the front
     res.send(_apiData);
   }
 );
-
-// handle static file middleware
-myApp.use(express.static("public"));
 
 // ability to choose available port to use
 // good practice
