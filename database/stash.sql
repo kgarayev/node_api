02d66e8ca52995e7267186aef425a49ffd915737
kgarayev <kenangarayev@gmail.com>
2023-07-22 22:54:18 +0100

    database added

--- database/stash.sql
+++ database/stash.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2023 at 09:41 PM
+-- Generation Time: Jul 22, 2023 at 11:53 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -28,15 +28,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `first_name` text NOT NULL,
-  `last_name` text NOT NULL,
-  `number` text NOT NULL,
-  `email` text NOT NULL,
+  `id` int(255) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `number` varchar(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `dob` date NOT NULL,
-  `password` text NOT NULL,
-  `created` date NOT NULL DEFAULT current_timestamp()
+  `password` varchar(50) NOT NULL,
+  `created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `number`, `email`, `dob`, `password`, `created`) VALUES
+(1000, 'Richard', 'Sanchez', '078624566', 'ricksanchez@gmail.com', '1900-02-10', 'Rick12345', '2023-07-22 00:00:00'),
+(1011, 'Peter', 'Griffin', '07765753455', 'dfgdfg@gmail.com', '1968-01-01', 'Peter1234', '2023-07-22 22:42:15'),
+(1015, 'Homer', 'Simpson', '07765756455', 'peter@gmail.com', '1968-01-01', 'MATA123rfG', '2023-07-22 22:44:59');
 
 --
 -- Indexes for dumped tables
@@ -46,7 +55,10 @@ CREATE TABLE `users` (
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `number` (`number`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -56,7 +68,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1000;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1016;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
