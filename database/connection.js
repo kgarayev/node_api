993fd5c3243cd032c08fe9b0da41191e547fd504
kgarayev <kenangarayev@gmail.com>
2023-07-22 11:33:34 +0100

    creating database programmatically if it does not exist

--- database/connection.js
+++ database/connection.js
@@ -4,12 +4,12 @@ const mysql = require("mysql");
 // instance of a connection
 // this driver does not support promises
 // need to create our own wrapper / promisify this
+// connect to RDMS without specifying the database itself
 const myConnection = mysql.createConnection({
   user: "root",
   password: "",
   host: "localhost",
   port: 3306,
-  database: "stash",
 });
 
 // wrapping the query code in the promise to make it async await compatible
@@ -20,11 +20,34 @@ const asyncMySQL = (query) => {
     myConnection.query(query, (error, results) => {
       if (error) {
         reject(error);
+        return;
       }
       resolve(results);
+      return;
     });
   });
 };
+
+// create a database function if one does not already exist
+const createDB = async (dbName) => {
+  try {
+    // Create the database if it doesn't exist
+    await asyncMySQL(`CREATE DATABASE IF NOT EXISTS ${dbName}`);
+
+    // Switch to using the new database
+    myConnection.changeUser({ database: dbName }, (error, results) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// create a database called stash
+createDB("stash");
 
 // exporting the function to be used elsewhere on the project
 module.exports = asyncMySQL;
